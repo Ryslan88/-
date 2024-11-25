56a57c7cec31c67a873d977fe227036214dc0213
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-11-25 18:54:07 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <vector>
+#include <limits>
+#include "Tvarina.h"
+#include "Khizhak.h"
+#include "Travoidni.h"
+#include "display.h"
+
+int main() {
+    std::vector<Tvarina*> tvarini;
+    int predatorCount = 0, herbivoreCount = 0;
+
+    // Введення кількості хижаків
+    while (true) {
+        std::cout << "Скільки хижаків ви хочете додати? ";
+        std::cin >> predatorCount;
+        if (std::cin.fail() || predatorCount <= 0) {
+            std::cout << "Введіть дійсне ціле число більше 0.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            break;
+        }
+    }
+
+    // Введення кількості травоїдних
+    while (true) {
+        std::cout << "Скільки травоїдних ви хочете додати? ";
+        std::cin >> herbivoreCount;
+        if (std::cin.fail() || herbivoreCount <= 0) {
+            std::cout << "Введіть дійсне ціле число більше 0.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            break;
+        }
+    }
+
+    // Введення хижаків
+    for (int i = 0; i < predatorCount; i++) {
+        std::cout << "\nВведення даних для хижака №" << (i + 1) << ":\n";
+        Khizhak* khizhak = new Khizhak();
+        khizhak->input();
+        tvarini.push_back(khizhak);
+    }
+
+    // Введення травоїдних
+    for (int i = 0; i < herbivoreCount; i++) {
+        std::cout << "\nВведення даних для травоїдної №" << (i + 1) << ":\n";
+        Travoidni* travoidni = new Travoidni();
+        travoidni->input();
+        tvarini.push_back(travoidni);
+    }
+
+    Display display;
+
+    // Виведення даних
+    for (const auto& t : tvarini) {
+        display.show(*t);
+        if (auto* khizhak = dynamic_cast<Khizhak*>(t)) {
+            display.showPredatorInfo(*khizhak);
+        } else if (auto* travoidni = dynamic_cast<Travoidni*>(t)) {
+            display.showHerbivoreInfo(*travoidni);
+        }
+        std::cout << "---------------------\n";
+    }
+
+    // Очищення пам'яті
+    for (auto t : tvarini) {
+        delete t;
+    }
+
+    return 0;
+}
