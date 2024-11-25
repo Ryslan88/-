52b9184642d667a52d46e8f1edf36f7990fda02b
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-11-25 18:55:02 +0200

    Create Khizhak.cpp

---
+++ Khizhak.cpp
@@ -0,0 +1,30 @@
+#include "Khizhak.h"
+#include <iostream>
+#include <limits>
+
+Khizhak::Khizhak(std::string n, double a, int d, int s)
+    : Tvarina(n, a, d), strength(s) {}
+
+Khizhak::~Khizhak() {}
+
+void Khizhak::input() {
+    Tvarina::input();
+
+    while (true) {
+        std::cout << "Введіть силу хижака: ";
+        std::cin >> strength;
+        if (std::cin.fail() || strength <= 0) {
+            std::cout << "Сила має бути числом більше 0. Спробуйте ще раз.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            break;
+        }
+    }
+}
+
+void Khizhak::display() const {
+    std::cout << "Хижак: " << name << "\n"
+              << "Вік: " << age << "\n"
+              << "Тривалість життя: " << duration << " років\n";
+}
