2c0d7d8df0102f79d6e1abf5de684d591c571e12
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-11-25 18:56:26 +0200

    Create Travoidni.h

---
+++ Travoidni.h
@@ -0,0 +1,21 @@
+#ifndef TRAVOIDNI_H
+#define TRAVOIDNI_H
+
+#include "Tvarina.h"
+
+class Travoidni : public Tvarina {
+private:
+    double weight; // Вага травоїдної
+
+public:
+    Travoidni(std::string n = "", double c = 0.0, int d = 0, double w = 0.0);
+    ~Travoidni();
+
+    friend class Display;
+
+    void display() const override;
+    void input() override; // Метод для введення даних
+    double getWeight() const { return weight; }
+};
+
+#endif
