416748454de22defbadcc960ed0b7dfbce73b3fe
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-11-25 18:54:33 +0200

    Create display.h

---
+++ display.h
@@ -0,0 +1,24 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include "Tvarina.h"
+#include "Khizhak.h"
+#include "Travoidni.h"
+#include <iostream>
+
+class Display {
+public:
+    void show(const Tvarina& tvarina) const {
+        tvarina.display();
+    }
+
+    void showPredatorInfo(const Khizhak& khizhak) const {
+        std::cout << "Сила хижака: " << khizhak.getStrength() << std::endl;
+    }
+
+    void showHerbivoreInfo(const Travoidni& travoidni) const {
+        std::cout << "Вага травоїдної: " << travoidni.getWeight() << " кг" << std::endl;
+    }
+};
+
+#endif
