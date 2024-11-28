980d983bcf0f416b67f43b6f182cd8f4bf908f47
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-11-28 15:50:11 +0200

    Update Khizhak.cpp

--- Khizhak.cpp
+++ Khizhak.cpp
@@ -9,27 +9,15 @@ Khizhak::~Khizhak() {}
 
 void Khizhak::input() {
     Tvarina::input();
-
-    while (true) {
-        std::cout << "Введіть силу хижака: ";
-        std::cin >> strength;
-        if (std::cin.fail() || strength <= 0) {
-            std::cout << "Сила має бути числом більше 0. Спробуйте ще раз.\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        } else {
-            break;
-        }
+    std::cout << "Введіть силу хижака: ";
+    while (!(std::cin >> strength) || strength <= 0) {
+        std::cout << "Сила має бути числом більше 0. Спробуйте ще раз: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 }
 
 void Khizhak::display() const {
-    std::cout << "Хижак: " << name << "\n"
-              << "Вік: " << age << "\n"
-              << "Тривалість життя: " << duration << " років\n";
-}
-
-// Дружня функція для скидання сили хижака
-void resetStrength(Khizhak& khizhak) {
-    khizhak.strength = 0;
+    Tvarina::display();
+    std::cout << "Сила: " << strength << '\n';
 }
