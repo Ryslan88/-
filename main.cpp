10bb6949c13bd547c9135077840a7ba22c378aaf
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-11-28 15:49:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,69 +8,41 @@
 
 int main() {
     std::vector<Tvarina*> tvarini;
-    int predatorCount = 0, herbivoreCount = 0;
+    int predatorCount, herbivoreCount;
 
-    // Введення кількості хижаків
-    while (true) {
-        std::cout << "Скільки хижаків ви хочете додати? ";
-        std::cin >> predatorCount;
-        if (std::cin.fail() || predatorCount <= 0) {
-            std::cout << "Введіть дійсне ціле число більше 0.\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        } else {
-            break;
-        }
+    std::cout << "Скільки хижаків ви хочете додати? ";
+    while (!(std::cin >> predatorCount) || predatorCount <= 0) {
+        std::cout << "Введіть дійсне ціле число більше 0: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
-    // Введення кількості травоїдних
-    while (true) {
-        std::cout << "Скільки травоїдних ви хочете додати? ";
-        std::cin >> herbivoreCount;
-        if (std::cin.fail() || herbivoreCount <= 0) {
-            std::cout << "Введіть дійсне ціле число більше 0.\n";
-            std::cin.clear();
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        } else {
-            break;
-        }
+    std::cout << "Скільки травоїдних ви хочете додати? ";
+    while (!(std::cin >> herbivoreCount) || herbivoreCount <= 0) {
+        std::cout << "Введіть дійсне ціле число більше 0: ";
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
     }
 
-    // Введення хижаків
-    for (int i = 0; i < predatorCount; i++) {
-        std::cout << "\nВведення даних для хижака №" << (i + 1) << ":\n";
+    for (int i = 0; i < predatorCount; ++i) {
         Khizhak* khizhak = new Khizhak();
         khizhak->input();
         tvarini.push_back(khizhak);
     }
-
-    // Введення травоїдних
-    for (int i = 0; i < herbivoreCount; i++) {
-        std::cout << "\nВведення даних для травоїдної №" << (i + 1) << ":\n";
+    for (int i = 0; i < herbivoreCount; ++i) {
         Travoidni* travoidni = new Travoidni();
         travoidni->input();
         tvarini.push_back(travoidni);
     }
 
     Display display;
-
-    // Виведення даних
-    for (const auto& t : tvarini) {
+    for (Tvarina* t : tvarini) {
         display.show(*t);
-        if (auto* khizhak = dynamic_cast<Khizhak*>(t)) {
-            display.showPredatorInfo(*khizhak);
-            display.resetPredatorStrength(*khizhak); // Скидаємо силу хижака
-        } else if (auto* travoidni = dynamic_cast<Travoidni*>(t)) {
-            display.showHerbivoreInfo(*travoidni);
-            display.resetHerbivoreWeight(*travoidni); // Скидаємо вагу травоїдної
-        }
+        display.reset(*t); // Скидання значень
         std::cout << "---------------------\n";
     }
 
-    // Очищення пам'яті
-    for (auto t : tvarini) {
-        delete t;
-    }
+    for (Tvarina* t : tvarini) delete t;
 
     return 0;
 }
