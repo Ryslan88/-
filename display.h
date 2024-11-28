23d10cb366c72928616865f72b75f2208f8b94e7
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-11-28 15:48:49 +0200

    Update display.h

--- display.h
+++ display.h
@@ -9,25 +9,32 @@
 class Display {
 public:
     void show(const Tvarina& tvarina) const {
-        tvarina.display();
+        tvarina.display(); // Виклик методу display() об'єкта
     }
 
-    void showPredatorInfo(const Khizhak& khizhak) const {
-        std::cout << "Сила хижака: " << khizhak.getStrength() << std::endl;
+    void showDetailed(const Khizhak& khizhak) const {
+        std::cout << "Детальна інформація про хижака:\n"
+                  << "Назва: " << khizhak.name << "\n"
+                  << "Сила: " << khizhak.strength << "\n"
+                  << "Тривалість життя: " << khizhak.duration << " років\n";
     }
 
-    void showHerbivoreInfo(const Travoidni& travoidni) const {
-        std::cout << "Вага травоїдної: " << travoidni.getWeight() << " кг" << std::endl;
+    void showDetailed(const Travoidni& travoidni) const {
+        std::cout << "Детальна інформація про травоїдну:\n"
+                  << "Назва: " << travoidni.name << "\n"
+                  << "Вага: " << travoidni.weight << " кг\n"
+                  << "Тривалість життя: " << travoidni.duration << " років\n";
     }
 
-    void resetPredatorStrength(Khizhak& khizhak) const {
-        resetStrength(khizhak);
-        std::cout << "Сила хижака скинута.\n";
-    }
-
-    void resetHerbivoreWeight(Travoidni& travoidni) const {
-        resetWeight(travoidni);
-        std::cout << "Вага травоїдної скинута.\n";
+    void reset(Tvarina& tvarina) const {
+        // Динамічне приведення типу для роботи з похідними класами
+        if (Khizhak* khizhak = dynamic_cast<Khizhak*>(&tvarina)) {
+            khizhak->strength = 0;
+            std::cout << "Сила хижака скинута до 0.\n";
+        } else if (Travoidni* travoidni = dynamic_cast<Travoidni*>(&tvarina)) {
+            travoidni->weight = 0;
+            std::cout << "Вага травоїдної скинута до 0.\n";
+        }
     }
 };
 
