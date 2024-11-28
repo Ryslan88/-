75d66ff782c8a4177ccb53d75650019152fa8db9
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-11-28 15:48:34 +0200

    Update Tvarina.h

--- Tvarina.h
+++ Tvarina.h
@@ -3,6 +3,8 @@
 
 #include <string>
 
+class Display;
+
 class Tvarina {
 protected:
     std::string name; // Назва тварини
@@ -13,16 +15,10 @@ public:
     Tvarina(std::string n = "", double a = 0.0, int d = 0);
     virtual ~Tvarina();
 
+    friend class Display; // Дружній клас
+
     virtual void display() const = 0; // Чисто віртуальний метод
-    virtual void input(); // Метод для введення даних
-
-    std::string getName() const;
-    double getAge() const;
-    int getDuration() const;
-
-    void setName(std::string n);
-    void setAge(double a);
-    void setDuration(int d);
+    virtual void input();             // Метод для введення даних
 };
 
 #endif
