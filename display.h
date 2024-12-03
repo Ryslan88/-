d3e9add59d2556293fd7d84f8481db7729335b0b
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-12-04 01:15:40 +0200

    Update display.h

--- display.h
+++ display.h
@@ -1,41 +1,22 @@
-#ifndef DISPLAY_H
-#define DISPLAY_H
+#ifndef KHIZHAK_H
+#define KHIZHAK_H
 
 #include "Tvarina.h"
-#include "Khizhak.h"
-#include "Travoidni.h"
-#include <iostream>
+#include "Display.h"
 
-class Display {
+
+class Khizhak : public Tvarina {
+private:
+    int strength; // Сила хижака
+
 public:
-    void show(const Tvarina& tvarina) const {
-        tvarina.display(); // Виклик методу display() об'єкта
-    }
+    Khizhak(std::string n = "", double c = 0.0, int d = 0, int s = 0);
+    ~Khizhak();
 
-    void showDetailed(const Khizhak& khizhak) const {
-        std::cout << "Детальна інформація про хижака:\n"
-                  << "Назва: " << khizhak.name << "\n"
-                  << "Сила: " << khizhak.strength << "\n"
-                  << "Тривалість життя: " << khizhak.duration << " років\n";
-    }
+    friend class Display; // Дружній клас
 
-    void showDetailed(const Travoidni& travoidni) const {
-        std::cout << "Детальна інформація про травоїдну:\n"
-                  << "Назва: " << travoidni.name << "\n"
-                  << "Вага: " << travoidni.weight << " кг\n"
-                  << "Тривалість життя: " << travoidni.duration << " років\n";
-    }
-
-    void reset(Tvarina& tvarina) const {
-        // Динамічне приведення типу для роботи з похідними класами
-        if (Khizhak* khizhak = dynamic_cast<Khizhak*>(&tvarina)) {
-            khizhak->strength = 0;
-            std::cout << "Сила хижака скинута до 0.\n";
-        } else if (Travoidni* travoidni = dynamic_cast<Travoidni*>(&tvarina)) {
-            travoidni->weight = 0;
-            std::cout << "Вага травоїдної скинута до 0.\n";
-        }
-    }
+    void display() const override;
+    void input() override; // Введення даних
 };
 
 #endif
