70d03ad0dba23eebef78846dde06d0799ec356f9
Ruslan88 <ruslan.nestor.ap.2023@lpnu.ua>
2024-12-04 01:16:15 +0200

    Update Khizhak.h

--- Khizhak.h
+++ Khizhak.h
@@ -1,8 +1,8 @@
 #ifndef KHIZHAK_H
 #define KHIZHAK_H
 
-#include "Tvarina.h"
-#include "Display.h"
+#include <string>  // Для std::string
+#include "Tvarina.h"  // Для базового класу Tvarina
 
 class Khizhak : public Tvarina {
 private:
@@ -11,8 +11,6 @@ private:
 public:
     Khizhak(std::string n = "", double c = 0.0, int d = 0, int s = 0);
     ~Khizhak();
-
-    friend class Display; // Дружній клас
 
     void display() const override;
     void input() override; // Введення даних
